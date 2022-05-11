a5c975c7ff81f3386a0bb81b8a1f0ef2da840c87
Flávio Bianchetti <fjb.developer@gmail.com>
2022-05-11 19:09:14 -0300

    feat: Structure form Login page included

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -15,6 +15,44 @@ const Login = () => {
   return (
     <div>
       <h1>Login</h1>
+      <form
+        data-testid="form"
+      >
+        <label htmlFor="email-label">
+          Email:
+          <input
+            className="email-input"
+            data-testid="input-email"
+            type="email"
+            value={ email }
+            onChange={(event) => setEmail(event.target.value)}
+            placeholder="Digite seu email"
+          />
+        </label>
+        <label htmlFor="password-label">
+          Senha:
+          <input
+            className="password-input"
+            data-testid="input-password"
+            type="password"
+            value={ password }
+            onChange={(event) => setPassword(event.target.value)}
+            placeholder="Digite sua senha"
+          />
+        </label>
+        <button
+          className="login-button"
+          data-testid="button-login"
+          type="submit"
+          disabled={!isValidEmail || !isValidPassword}
+        >
+          Entrar
+        </button>
+      </form>
+      { 
+        ((email.length > 0 && !isValidEmail) || (password.length > 0 && !isValidPassword)) 
+        && <p>É necessário um e-mail válido e uma senha com 6 caracteres ou mais.</p> 
+      }
     </div>
   );
 };
