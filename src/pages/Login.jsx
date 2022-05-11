a262840c59277643f9a9a7a1d794a2e1af39001c
Flávio Bianchetti <fjb.developer@gmail.com>
2022-05-11 18:43:38 -0300

    feat: Attributes useState included in Login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,11 @@
+import { useState } from 'react';
+
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isValidEmail, setIsValidEmail] = useState(false);
+  const [isValidPassword, setIsValidPassword] = useState(false);
+
   return (
     <div>
       <h1>Login</h1>
