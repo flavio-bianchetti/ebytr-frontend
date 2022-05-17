c3764ecf0ae9330b8589b77d8fd853a2f850de71
Flávio Bianchetti <fjb.developer@gmail.com>
2022-05-17 09:17:55 -0300

    feat: Import components in TodoList file

--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -1,8 +1,18 @@
+import React from 'react';
+import Header from '../components/Header';
+import TaskBar from '../components/TaskBar';
+import Table from '../components/Table';
+import Footer from '../components/Footer';
+
 const TodoList = () => {
+  
   return (
-    <div>
-      <h1>TodoList</h1>
-    </div>
+    <>
+      <Header />
+      <TaskBar />
+      <Table />
+      <Footer />
+    </>
   );
 };
 
