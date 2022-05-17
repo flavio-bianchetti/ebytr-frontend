5c81ed37a5312913b52ed0b9c5f1cd20b26e01d9
Flávio Bianchetti <fjb.developer@gmail.com>
2022-05-17 07:57:34 -0300

    test: Login test updated

--- src/tests/01-Login.test.js
+++ src/tests/01-Login.test.js
@@ -1,46 +1,49 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import { screen, render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
+import renderWithRouterProvider from '../renderWithRouterProvider';
 
 
 describe('1 - Crie uma página de login', () => {
-  const setup = () => render(
-    <React.StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </React.StrictMode>
-  );
-
   it ('Renderiza corretamente a página de login', () => {
-    setup();
-    expect(screen.getByText('Login')).toBeInTheDocument();
+    const { getByText } = renderWithRouterProvider(<App />);
+    expect(getByText('Login')).toBeInTheDocument();
   });
 
   it('Renderiza corretamente o formulário de login', () => {
-    setup();
-    expect(screen.getByTestId('form')).toBeInTheDocument();
+    const { getByTestId } = renderWithRouterProvider(<App />);
+    expect(getByTestId('form')).toBeInTheDocument();
   });
 
   it('Renderiza corretamente o campo de digitação do email', () => {
-    setup();
-    expect(screen.getByTestId('input-email')).toBeInTheDocument();
+    const { getByTestId } = renderWithRouterProvider(<App />);
+    expect(getByTestId('input-email')).toBeInTheDocument();
   });
 
   it('Renderiza corretamente o campo de digitação de senha', () => {
-    setup();
-    expect(screen.getByTestId('input-password')).toBeInTheDocument();
+    const { getByTestId } = renderWithRouterProvider(<App />);
+    expect(getByTestId('input-password')).toBeInTheDocument();
   });
 
   it('Renderiza corretamente o botão "Entrar"', () => {
-    setup();
-    expect(screen.getByTestId('button-login')).toBeInTheDocument();
+    const { getByTestId } = renderWithRouterProvider(<App />);
+    expect(getByTestId('button-login')).toBeInTheDocument();
   });
 
   it('o botão "Entrar" está desabilitado', () => {
-    setup();
-    expect(screen.getByTestId('button-login')).toBeDisabled();
+    const { getByTestId } = renderWithRouterProvider(<App />);
+    expect(getByTestId('button-login')).toBeDisabled();
+  });
+
+  it('o botão "Entrar" fica habilitado após a inserção das informações corretas.', () => {
+    const { getByTestId } = renderWithRouterProvider(<App />);
+    const email = getByTestId('input-email');
+    const password = getByTestId('input-password');
+    const button = getByTestId('button-login');
+    userEvent.type(email, 'teste@test.com');
+    expect(button).toBeDisabled();
+    userEvent.type(password, '123456');
+    expect(button).not.toBeDisabled();
   });
 });
