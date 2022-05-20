90377ef4948420ff7c6ae3759c6335932623d74b
Flávio Bianchetti <fjb.developer@gmail.com>
2022-05-19 23:02:17 -0300

    feat: CreateUser & NotFound routes included

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import TodoList from './pages/TodoList';
+import CreateUser from './pages/CreateUser';
+import NotFound from './pages/NotFound';
 
 const App = () => {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
+      <Route path="/create" component={ CreateUser } />
       <Route path="/todolist" component={ TodoList } />
+      <Route path="*" component={ NotFound } />
     </Switch>
   );
 };
