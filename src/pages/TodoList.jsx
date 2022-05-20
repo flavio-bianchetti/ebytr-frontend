dde47c6d8948429882c314bfdf2e2a2bfe2e7854
Flávio Bianchetti <fjb.developer@gmail.com>
2022-05-19 22:47:35 -0300

    feat: Todo list page updated fields included

--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Header from '../components/Header';
 import TaskBar from '../components/TaskBar';
 import Table from '../components/Table';
 import Footer from '../components/Footer';
+import TodoListContext from '../context/TodoListContext';
+import { useHistory } from 'react-router-dom';
 
 const TodoList = () => {
-  
+  const { userInfo } = useContext(TodoListContext);
+  const history = useHistory();
   return (
     <>
+      { !userInfo.id && history.push('/')  }
       <Header />
       <TaskBar />
       <Table />
